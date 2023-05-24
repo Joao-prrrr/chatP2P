be9eeb48c8f46b920cada8a6b025433c64ac5a95
Joao-prrrr <joao.prrrr@eduge.ch>
2023-05-24 11:14:38 +0200

    testing

---
+++ chatP2P/mainChat.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace chatP2P
+{
+    public partial class mainChat : Form
+    {
+        public mainChat()
+        {
+            InitializeComponent();
+        }
+
+        async void test()
+        {
+            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("host.contoso.com");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+            using Socket client = new(
+            ipEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+            await client.ConnectAsync(ipEndPoint);
+            while (true)
+            {
+                // Send message.
+                var message = "Hi friends 👋!<|EOM|>";
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                if (response == "<|ACK|>")
+                {
+                    Console.WriteLine(
+                        $"Socket client received acknowledgment: \"{response}\"");
+                    break;
+                }
+                // Sample output:
+                //     Socket client sent message: "Hi friends 👋!<|EOM|>"
+                //     Socket client received acknowledgment: "<|ACK|>"
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+        }
+    }
+}
